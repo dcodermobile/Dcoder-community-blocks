2e6f74985a61dc8bb0a65f6c7a03f5c27defa3bb
Abhilipsa Sahoo <80174214+abhilipsasahoo03@users.noreply.github.com>
2022-02-08 15:56:39 +0530

    Update index.js

--- GetCovid19Statistics/index.js
+++ GetCovid19Statistics/index.js
@@ -1,12 +1,7 @@
+const covid19 = require('covid19-stats');
 const main = async (inputs, auths, context) => {
-
-    const covid19 = require('covid19-stats');
-    
-    const country = inputs.COUNTRY;
-
+    const country = inputs.country;
     var stats = await covid19.getCountry(country);
-
     return stats;
 }
-
 module.exports.main = main
