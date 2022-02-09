875b54303650ceacf9841e0df69593a62c83fcb1
nmghelani <nevilghelani99@gmail.com>
2022-02-09 18:05:58 -0500

    Using @vercel/ncc build for Canvas inbox block

--- GetCanvasInbox/index.js
+++ GetCanvasInbox/index.js
@@ -2,13 +2,18 @@ const axios = require("axios");
 const main = async(inputs, auths, context) => {
   console.log("Fetching data");
   let { domain, token } = inputs;
-  const {data} = await axios.get(
-    `https://${domain}/api/v1/conversations`,
-    {
-      headers: { 'Authorization': `Bearer ${token}` }
-    }
-  );
-  return data;
+  try{
+    const {data} = await axios.get(
+      `https://${domain}/api/v1/conversations`,
+      {
+        headers: { 'Authorization': `Bearer ${token}` }
+      }
+    );
+    console.log("Data fetched successfully");
+    return data;
+  } catch (e) {
+    console.log("Failed to fetch data: " + e.message);
+  }
 }
 
 module.exports.main = main
