01a934d543929ad26c46f5694eaf93f89ab071f8
nmghelani <nevilghelani99@gmail.com>
2022-02-09 17:51:06 -0500

    Using @vercel/ncc build and some other small changes

--- GetCanvasTODOList/index.js
+++ GetCanvasTODOList/index.js
@@ -2,13 +2,19 @@ const axios = require("axios");
 const main = async(inputs, auths, context) => {
   console.log("Fetching data");
   let { domain, token } = inputs;
-  const {data} = await axios.get(
-    `https://${domain}/api/v1/users/self/todo`,
-    {
-      headers: { 'Authorization': `Bearer ${token}` }
-    }
-  );
-  return data;
+  try{
+    const {data} = await axios.get(
+      `https://${domain}/api/v1/users/self/todo`,
+      {
+        headers: { 'Authorization': `Bearer ${token}` }
+      }
+    );
+    console.log("Data fetched successfully");
+    return data;
+  } catch(e) {
+    console.log("Failed to fetch data: " + e.message);
+  }
+  return [];
 }
 
 module.exports.main = main
