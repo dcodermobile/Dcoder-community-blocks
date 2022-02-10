f0a7f5bd3ee927f30bac3df0252417052596a6e3
theabgupta <abgupta.456@gmail.com>
2022-02-10 14:07:56 +0530

    Add files via upload

--- DropboxMoveFile/index.js
+++ DropboxMoveFile/index.js
@@ -1,6 +1,7 @@
 const main = async (inputs, auths, event) => {
   const fetch = require('isomorphic-fetch');
   const { Dropbox } = require('dropbox');
+
   const dbx = new Dropbox({
     accessToken: auths.DROPBOX.ACCESS_TOKEN,
     fetch
@@ -8,8 +9,8 @@ const main = async (inputs, auths, event) => {
 
   try {
     const resp = await dbx.filesMoveV2({
-      fromPath: inputs.fromPath,
-      toPath: inputs.toPath
+      from_path: inputs.fromPath,
+      to_path: inputs.toPath
     })
     return resp
   } catch (err) {
