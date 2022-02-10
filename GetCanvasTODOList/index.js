a8be15ca005b54723431c7949b9c7f566edb9f9d
nmghelani <nevilghelani99@gmail.com>
2022-02-10 09:14:26 -0500

    Clean code

--- GetCanvasTODOList/index.js
+++ GetCanvasTODOList/index.js
@@ -1,12 +1,15 @@
-const axios = require("axios");
-const main = async (inputs, auths, context) => {
-  console.log("Fetching data");
-  let { domain, token } = inputs;
-  const { data } = await axios.get(`https://${domain}/api/v1/users/self/todo`, {
-    headers: { Authorization: `Bearer ${token}` },
-  });
-  console.log("Data fetched successfully");
-  return data;
-};
+const axios = require("axios")
+const main = async(inputs, auths, context) => {
+  console.log("Fetching data")
+  let { domain, token } = inputs
+  const {data} = await axios.get(
+    `https://${domain}/api/v1/users/self/todo`,
+    {
+      headers: { 'Authorization': `Bearer ${token}` }
+    }
+  )
+  console.log("Data fetched successfully")
+  return data
+}
 
-module.exports.main = main;
+module.exports.main = main
